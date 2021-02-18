31a0571a26a1e5ec94d8c53e39479f65cf57e359
Koen Van Impe <koen.vanimpe@cudeso.be>
2021-02-18 08:10:06 +0100

    Include NGINX and Cacti

--- init-elastic-dfir.sh
+++ init-elastic-dfir.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Shut down old containers, remove orphaned containers
 docker-compose down -v --remove-orphans
@@ -6,6 +6,13 @@ docker-compose down -v --remove-orphans
 # Remove volumes
 docker volume rm elastic_data01
 docker volume rm elastic_data02
+docker volume rm data01 
+docker volume rm data02 
+docker volume rm cacti-db 
+docker volume rm cacti-data
+docker volume rm cacti-spine
+docker volume rm cacti-backups
+
 
 # You might also want to init networks and volumes
 docker network prune
--- start-elastic-dfir.sh
+++ start-elastic-dfir.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Start dockers in screen, detach from screen
 screen -S elastic_dfir -dm docker-compose up
